ecbd58a3c9c39550708f57f1cebef4507b667937
404neko <404neko@users.noreply.github.com>
2025-03-24 23:46:08 +0800

    Delete new  2.m

--- new  2.m
+++
@@ -1,6 +0,0 @@
-http://union.bokecc.com/playvideo.bo?uid=198610D610BE6FCA&playertype=1&autoStart=false&vid=517F97796A3B84D7
-http://union.bokecc.com/playvideo.bo?&playertype=1&autoStart=false&vid=E5C04EF2B3D0C9509C33DC5901307461
-
-cc_js_Player.showPlayerView({"path":"","status":1,"vid":"517F97796A3B84D7","uid":"213562","divid":"cc_video_517F97796A3B84D7_2068812"});
-
-cc_js_Player.showPlayerView({"path":"","status":1,"vid":"E5C04EF2B3D0C9509C33DC5901307461","uid":"215893","divid":"cc_video_E5C04EF2B3D0C9509C33DC5901307461_4920601"});
